e4d1d21beba86782cd7d581b7c233f319564d8c3
Дмитрий Баня <banyadima992@gmail.com>
2019-09-09 12:10:32 +0300

    start

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomList;
+using NConsoleGraphics;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
